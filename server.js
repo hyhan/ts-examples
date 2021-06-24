f8d69d045c034d52ca80ac6b3da031068cca7e8f
hui <125173679@qq.com>
2021-06-24 17:10:07 +0800

    修改调用

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ registerMoreRouter()
 
 app.use(router)
 
-const port = process.env.PORT || 8080
+const port = 8817
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
